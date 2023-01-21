fd8fc278033afbeb5d584e16764680386444cdfe
amo-draper <amoghpreneur@gmail.com>
2023-01-21 20:58:25 +0530

    all set

--- script.js
+++ script.js
@@ -14,59 +14,87 @@ const result = fetch(
   });
 
 // create a function to create a card for each article and append it to the DOM to display the news
+
 function createCard(article) {
+  
+  let marquee = document.querySelector('marquee');
+  marquee.innerHTML +=   (article.title).toUpperCase() + "   ||   " ;
+
   // create a div element to hold the card
-  var card = document.createElement("div");
+
+  let card = document.createElement("div");
   card.classList.add("card");
-  // create a div element to hold the card image
-  var cardImage = document.createElement("div");
-  cardImage.classList.add("card-image");
+  
   // create an image element to hold the image
-  var image = document.createElement("img");
+  
+  let image = document.createElement("img");
+  image.classList.add("card-image")
   image.src = article.urlToImage;
+  
   // create a div element to hold the card content
-  var cardContent = document.createElement("div");
+  
+  let cardContent = document.createElement("div");
   cardContent.classList.add("card-content");
+  
   // create a span element to hold the title
-  var title = document.createElement("span");
+  
+  let title = document.createElement("span");
   title.classList.add("card-title");
   title.textContent = article.title;
+  
   // create a p element to hold the description
-  var description = document.createElement("p");
+  
+  let description = document.createElement("p");
   let root = document.querySelector(':root');
   let rootCss = getComputedStyle(root);  
   let trunc = rootCss.getPropertyValue('--setTrunc')
+  
   description.textContent = article.description;
+  
   if(trunc == 1) 
-  description.textContent = description.textContent.substring(0,50);
+    description.textContent = description.textContent.substring(0,50);
+  
+  description.textContent += '...';
   // create a div element to hold the card action
-  var cardAction = document.createElement("div");
+  
+  let cardAction = document.createElement("div");
   cardAction.classList.add("card-action");
+  
   // create an anchor element to hold the link to the article
-  var link = document.createElement("a");
+  
+  let link = document.createElement("a");
   link.textContent = "Read More";
   link.href = article.url;
+  
   // append the elements to the DOM
+  
   cardAction.appendChild(link);
   cardContent.appendChild(title);
   cardContent.appendChild(description);
-  cardImage.appendChild(image);
-  card.appendChild(cardImage);
+  card.appendChild(image);
   card.appendChild(cardContent);
   card.appendChild(cardAction);
   document.querySelector("#news").appendChild(card);
 }
 
 // create a function to display the news
+
 function displayNews(data) {
+
   // loop through the articles and create a card for each article
-  for (var i = 0; i < data.articles.length; i++) {
+
+  for (let i = 0; i < data.articles.length; i++) {
     createCard(data.articles[i]);
   }
+
 }
+
 // create a function to clear the news
+
 function clearNews() {
   // select the news div and set its innerHTML to an empty string
+
   document.querySelector("#news").innerHTML = "";
+
 }
 
